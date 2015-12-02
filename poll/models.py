2a8b1aadafe8403918e50b58ea570e940639252d
giraffe0813 <yemengying1993@foxmail.com>
2015-12-02 20:37:21 +0800

    test gitignore

--- poll/models.py
+++ poll/models.py
@@ -4,6 +4,9 @@ from django.db import models
 from django.utils import timezone
 
 
+
+
+
 # Create your models here.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
