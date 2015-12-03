6147452a163d0554eb609b7cf0b7a552f1e030b0
giraffe0813 <yemengying1993@foxmail.com>
2015-12-03 18:29:13 +0800

    complete part 3 of tutorial, add some ugly views,haha

--- poll/views.py
+++ poll/views.py
@@ -1,18 +1,20 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, Http404
 from .models import Question
 
 
 # Create your views here.
 def index(request):
     latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    output = ', '.join(p.question_text for p in latest_question_list)
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question':question})
 
 
 def results(request, question_id):
