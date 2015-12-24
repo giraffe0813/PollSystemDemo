adda10ba907ccab5b754efd90651994650274b73
giraffe0813 <yemengying1993@foxmail.com>
2015-12-24 12:58:37 +0800

    merge gugang's code

--- mysite/settings.py
+++ mysite/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'mydatabase',
         'USER': 'root',
-        'PASSWORD': '',
+        'PASSWORD': 'coffee',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
